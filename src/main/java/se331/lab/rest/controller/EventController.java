12ca5963c5e5b14fed82f2fcb342663741ad0acf
Kongpj <pasakon_paingjai@cmu.ac.th>
2021-08-26 01:31:55 +0700

    3.2 use try catch to detect outofbound

--- src/main/java/se331/lab/rest/controller/EventController.java
+++ src/main/java/se331/lab/rest/controller/EventController.java
@@ -84,16 +84,22 @@ public class EventController {
                 .organizer("Brody Kill")
                 .build());
     }
+
     @GetMapping("events")
-    public ResponseEntity<?> getEventLists(@RequestParam(value = "_limit",required = false)Integer perPage,
-                                           @RequestParam(value = "_page",required = false)Integer page){
-        perPage = perPage == null?eventList.size() : perPage;
-        page = page == null?1:page;
-        Integer firstIndex = (page-1)*perPage;
+    public ResponseEntity<?> getEventLists(@RequestParam(value = "_limit", required = false) Integer perPage,
+                                           @RequestParam(value = "_page", required = false) Integer page) {
+        perPage = perPage == null ? eventList.size() : perPage;
+        page = page == null ? 1 : page;
+        Integer firstIndex = (page - 1) * perPage;
         List<Event> output = new ArrayList<>();
-        for (int i = firstIndex; i<firstIndex+perPage;i++){
-            output.add(eventList.get(i));
+        try {
+            for (int i = firstIndex; i < firstIndex + perPage; i++) {
+                output.add(eventList.get(i));
+            }
+        }catch (IndexOutOfBoundsException ex){
+            return ResponseEntity.ok(output);
         }
+
         return ResponseEntity.ok(output);
     }
 }
